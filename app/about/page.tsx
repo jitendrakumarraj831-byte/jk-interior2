3663419bf0bc979f44da94f016c67b61c95c3226
jitendrakumarraj831-byte <jitendrakumarraj831@gmail.com>
2026-04-03 12:32:24 +0530

    Create page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navbar"
+import WhyUs from "@/components/why-us"
+import Footer from "@/components/footer"
+
+export default function Page() {
+  return (
+    <>
+      <Navbar />
+      <WhyUs />
+      <Footer />
+    </>
+  )
+}
