ea29290a4b2788c7309948657ed551645856ae7a
jitendrakumarraj831-byte <jitendrakumarraj831@gmail.com>
2026-04-03 12:33:12 +0530

    Create page.tsx

---
+++ app/gallery/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navbar"
+import Gallery from "@/components/gallery"
+import Footer from "@/components/footer"
+
+export default function Page() {
+  return (
+    <>
+      <Navbar />
+      <Gallery />
+      <Footer />
+    </>
+  )
+}
