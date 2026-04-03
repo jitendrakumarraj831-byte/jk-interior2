d7eb1f6ff95120321b618d59b68f860b59a360df
jitendrakumarraj831-byte <jitendrakumarraj831@gmail.com>
2026-04-03 12:33:51 +0530

    Create page.tsx

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navbar"
+import Contact from "@/components/contact"
+import Footer from "@/components/footer"
+
+export default function Page() {
+  return (
+    <>
+      <Navbar />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
