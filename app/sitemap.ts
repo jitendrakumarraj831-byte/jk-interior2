e3198897703bbcc2964389917c8a5c013341b147
jitendrakumarraj831-byte <jitendrakumarraj831@gmail.com>
2026-04-04 09:08:32 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,10 +2,10 @@ import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    { url: 'https://jkinterior.online' },
-    { url: 'https://jkinterior.online/services' },
-    { url: 'https://jkinterior.online/about' },
-    { url: 'https://jkinterior.online/gallery' },
-    { url: 'https://jkinterior.online/contact' },
+    { url: 'https://www.jkinterior.online' },
+    { url: 'https://www.jkinterior.online/services' },
+    { url: 'https://www.jkinterior.online/about' },
+    { url: 'https://www.jkinterior.online/gallery' },
+    { url: 'https://www.jkinterior.online/contact' },
   ]
 }
